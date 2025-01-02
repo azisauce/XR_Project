4350de034ffb8cc71c7ae9dddede18104d035d8e
root <root@ar-project.piiafg2abafuphvluherwy1ykg.parx.internal.cloudapp.net>
2025-01-02 13:37:36 +0000

    ADD: project architecture

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'exercise-core-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/images/favicon.png',
+  '/images/favicon.svg'
+];
+
+// Install service worker and cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Serve files from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate service worker and clear old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
